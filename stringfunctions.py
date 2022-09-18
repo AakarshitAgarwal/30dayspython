cdfc50712424bbf7c936ed4257f65500dee041bb
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-19 02:10:18 +0530

    updating py code

---
+++ stringfunctions.py
@@ -0,0 +1,11 @@
+mystr = "my name is Aakarshit Agarwal"
+print(len(mystr))
+print(mystr[::-1])
+print(type(mystr))
+print(mystr.endswith("Agarwal"))
+print(mystr.count("a"))
+print(mystr.capitalize())
+print(mystr.find("is"))
+print(mystr.lower())
+print(mystr.upper())
+print(mystr.replace("is","are"))
