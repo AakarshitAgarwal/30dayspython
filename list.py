4d00bca2cef8e9ec5116c196eb9e69787228f04f
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-20 01:11:28 +0530

    mutable and immutable properties

--- list.py
+++ list.py
@@ -1,16 +1,27 @@
 # grocery = ["a","b","c",56]
 # print(grocery)
-numbers = [2,7,9,11,3]
+# numbers = [2,7,9,11,3]
 # numbers.sort()
 # numbers.reverse()
 # print(numbers[1:4:-3])  # will not work
-numbers.append(69)
-numbers.insert(1,77)
-numbers.remove(9)
-numbers.pop()
-print(numbers)
-numbers[1]= 7
-print(numbers)
+# numbers.append(69)
+# numbers.insert(1,77)
+# numbers.remove(9)
+# numbers.pop()
+
+
+# print(numbers)   # list are mutables
+# numbers[1]= 7
+# print(numbers)
+
+
 # tup = (1,4,5,6)
 # tup[1] = 8     # tuples are immutables
 # print(tup)
+
+
+# reverse numbers in python
+a = 1
+b = 2
+a,b = b,a
+print(a,b)
