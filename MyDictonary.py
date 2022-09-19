a2a8fbb5819e46f2bc649460335650bfc345dd33
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-20 01:43:57 +0530

    My dictonary example

---
+++ MyDictonary.py
@@ -0,0 +1,5 @@
+mydictionary = {"Aakarshit":"attractive","Isha":"Parvati","Sakshi":"Kanuun"}
+print("Which word you want meaning from my dictonary : ", mydictionary)
+word=input()
+# print(mydictionary.get(word))
+print(mydictionary[word])
