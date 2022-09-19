6a1b4eff0ddad6ab356f48f82f0a6167d7095555
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-20 01:05:31 +0530

    list

--- list.py
+++ list.py
@@ -3,4 +3,11 @@
 numbers = [2,7,9,11,3]
 # numbers.sort()
 # numbers.reverse()
-print(numbers[1:4:-3])  # will not work
+# print(numbers[1:4:-3])  # will not work
+numbers.append(69)
+numbers.insert(1,77)
+numbers.remove(9)
+numbers.pop()
+print(numbers)
+tup = (1,)
+print(tup)
