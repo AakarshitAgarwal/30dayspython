233fc97ba031eeef88b4d530bc68a7f2d125d8a3
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-21 01:15:44 +0530

    if else in python

---
+++ ifelse.py
@@ -0,0 +1,19 @@
+# var1 = 25 
+
+# var2 = int(input())
+
+# if var1>var2:
+#     print("var2 is smaller")
+# elif var1 == var2:
+#     print("var1 is equal to var2")
+# else:
+#     print("var2 is greater")    
+
+
+
+list1 = [5,7,3]
+print(15 in list1)
+if 15 in list1:
+    print("Yes its in the list")
+else:
+    print("No it is not in list")
