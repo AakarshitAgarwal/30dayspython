a5c3f76837f0e04639963e67fa8c9bc2ed208d32
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-22 03:36:06 +0530

    input output in file

--- InputOutput.py
+++ InputOutput.py
@@ -1,22 +1,33 @@
-from turtle import rt
+# # f =open("sample.txt","rt")
+# # print(f.readlines())
+# # print(f.readlines())
+# # print(f.readlines())
+
+# f = open("sample.txt","rt")
 
 
-# f =open("sample.txt","rt")
-# print(f.readlines())
-# print(f.readlines())
-# print(f.readlines())
-
-f = open("sample.txt","rt")
+# # content = f.read()
+# for line in f:
+#     print(line)
 
 
-# content = f.read()
-for line in f:
-    print(line)
+# # print(content)
+
+# # content = f.read(3)
+# # print(content)
+
+# f.close()
 
 
-# print(content)
+# #write mode
+# f=open("sample.txt","w")
 
-# content = f.read(3)
-# print(content)
+# f.write("Aakarshit is trying hands on python")
+# f.close
 
-f.close()
+#append mode
+f=open("sample.txt","a")
+
+a =  f.write("Aakarshit is trying hands on python")
+print(a)
+f.close
