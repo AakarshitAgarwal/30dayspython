7d460be7bbc485eb4c20cb9c16a18ab15bc15eb2
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-22 01:55:47 +0530

    functions and docstring

---
+++ functons.py
@@ -0,0 +1,14 @@
+# def funtion1():
+#     print("Hi my name is aakarshit")
+
+# funtion1()
+
+def function1(a,b):
+    """ This function help us to find the average of two numbers """
+    average = (a+b)/2
+    return average
+
+
+# v = function1(5,7)
+# print(v)
+print(function1.__doc__)
