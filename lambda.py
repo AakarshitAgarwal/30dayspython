0e19244565acc1faddc01a9f3c99e5eef9ff87c3
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-24 01:56:55 +0530

    lambda functions

---
+++ lambda.py
@@ -0,0 +1,16 @@
+#anonymous functions or lambda functions -> these are the one liner functions
+
+
+# def add(a,b):
+#     return a-b
+
+
+# add = lambda a,b:a-b
+
+# print(add(7,2))    
+
+
+
+a=[[1,24],[7,243],[8,34]]
+a.sort(key=lambda x:x[1])
+print(a)
