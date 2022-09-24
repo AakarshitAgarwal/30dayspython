a48f29854c5017d2ace225fb3b94fb0de2c92420
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-24 23:38:43 +0530

    local time print and sleep

--- timemodule.py
+++ timemodule.py
@@ -1,14 +1,21 @@
 import time
 
-initial =time.time()
+# initial =time.time()
 
-k=0
-while(k<10):
-    print("This is Aakarshit")
-    k+=1
-print("While loop runs in", time.time() - initial,"seconds")
+# k=0
+# while(k<10):
+#     print("This is Aakarshit")
+#     # time.sleep(2)
+#     k+=1
+# print("While loop runs in", time.time() - initial,"seconds")
 
-initial2=time.time()
-for i in range(10):
-    print("This is Aakarshit")
-print("For loop ran in",time.time() - initial2,"seconds")
+# initial2=time.time()
+# for i in range(10):
+#     print("This is Aakarshit")
+# print("For loop ran in",time.time() - initial2,"seconds")
+
+
+#print local time 
+
+localtime = time.asctime(time.localtime(time.time()))
+print(localtime)
