495b1f4a9e80786e47ed71aedb63d06617706fb2
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-25 21:03:22 +0530

    import use

---
+++ file.py
@@ -0,0 +1,3 @@
+a=10
+def printfun(str):
+    print(f"This is me Aakarshit {str}")
---
+++ importUSE.py
@@ -0,0 +1,13 @@
+# import sklearn as sk
+# print(sk.__version__)
+
+# import sys
+# print(sys.path)
+
+# import file
+# print(file.a)
+
+from file import a, printfun
+print(a)
+printfun("it is me")
+
