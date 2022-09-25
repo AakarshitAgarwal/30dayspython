3ac90e4cac5e1bf0979ab9b215efccc7567a4990
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-25 21:18:03 +0530

    usinng __name__ = '__main__'

--- tutmain1.py
+++ tutmain1.py
@@ -8,4 +8,4 @@ def add(num1 , num2):
 #adding main in the python file so that if file import this file than my function inside main will not get execute
 if __name__ == '__main__':
     o=add(4,5)
-    print(f"This is the total sum {0}")
+    print(f"This is the total sum {o}")
