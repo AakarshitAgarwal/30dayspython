5cc588aaef43a49d9e494e5aa388ba3d8cdc6379
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-27 02:24:49 +0530

    decorators in python

---
+++ decorators.py
@@ -0,0 +1,13 @@
+def dec1(func1):
+    def nowexec():
+        print("Executing now")
+        func1()
+        print("Executed")
+    return nowexec
+
+@dec1
+def who_is_harry():
+    print("Harry is the good boy")
+
+# who_is_harry = dec1(who_is_harry)  
+who_is_harry()  
