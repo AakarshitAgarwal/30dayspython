615a9032f3d567860ec8b3dda13910088db0f453
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-27 02:48:33 +0530

    OOPS Concepts

---
+++ OOPS.py
@@ -0,0 +1,12 @@
+class Students:
+    pass
+
+harry = Students()
+larry = Students()
+
+harry.name = "Harry"
+larry.names = ["Aakarshit","Anurag","Utkarsh"]
+harry.std = 3
+
+
+print(harry.std,larry.names)
