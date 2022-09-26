93012e7b5757d891e7c02cf35ebd387b029f01ed
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-27 03:45:03 +0530

    self and __init__ in python

--- OOPS.py
+++ OOPS.py
@@ -1,12 +1,33 @@
-class Students:
-    pass
+# class Students:
+#     no_of_leaves = 8
+#     pass
 
-harry = Students()
-larry = Students()
+# harry = Students()
+# larry = Students()
 
-harry.name = "Harry"
-larry.names = ["Aakarshit","Anurag","Utkarsh"]
-harry.std = 3
+# harry.name = "Harry"
+# larry.names = ["Aakarshit","Anurag","Utkarsh"]
+# harry.std = 3
 
 
-print(harry.std,larry.names)
+# print(Students.no_of_leaves)
+# print(Students.__dict__)
+
+# larry.no_of_leaves = 10
+# print(larry.no_of_leaves)
+
+
+class Employee:
+    no_of_leaves = 9
+
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):
+        return f"The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+
+
+harry = Employee("Harry", 256, "Instructor")
+print(harry.salary)
