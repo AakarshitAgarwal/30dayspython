6335242e736ea7be930add2d91f90e5cdde4891e
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-09-28 03:35:30 +0530

    Class As Constructor

---
+++ ClassAsConstructor.py
@@ -0,0 +1,20 @@
+class Employee:
+    no_of_leaves = 8
+
+    def __init__(self,aname,asalary,arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    @classmethod
+    def from_dash(cls, newleaves):
+        cls.no_of_leaves = newleaves
+
+    @classmethod
+    def from_dash(cls,string):
+        return cls(*string.split("-"))
+
+
+karan = Employee.from_dash("Karan-420-Student") 
+
+print(karan.salary)
