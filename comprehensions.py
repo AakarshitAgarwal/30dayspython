982d47d65b257db3e1e07a59ad1911a2dc00f3b6
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2022-10-02 17:00:10 +0530

    comprehensions in python

--- comprehensions.py
+++ comprehensions.py
@@ -6,5 +6,14 @@
 
 # print(ls)
 
-ls = [i for i in range(100) if i%3==0]
-print(ls)
+# ls = [i for i in range(100) if i%3==0]
+# print(ls)
+
+# Not a SMATER WAY
+# dict1 = {0:"item0",1:"items1"}
+
+#SMATER WAY
+dict = {i:f"item{i}" for i in range(5,100) if i%3==0}
+#reverse Dictonary
+dict = { value:key for key,value in dict.items()}
+print(dict)
