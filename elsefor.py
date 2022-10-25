8aa4c10145efb1a0cab69addb7195ef1b919fb33
AakarshitAgarwal <aakarshit.agarwal@sprinklr.com>
2022-10-25 17:40:14 +0530

    else with for loop

---
+++ elsefor.py
@@ -0,0 +1,9 @@
+khana = ["roti","Sabzi","chawal"]
+
+for item in khana:
+    # if item == "paratha":
+    #     break
+    print(item)
+    break
+else:
+    print("Your item is not found in the kahan list")
